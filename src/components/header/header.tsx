0c62ee2ac56f7ce1a7bb4ba95fc35ec7f4ca35f1
Abdugani Abduqaxxorov <abduganiabdu2@gmail.com>
2023-05-05 10:28:26 +0500

    done

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,19 +1,18 @@
 import logo from "../../assets/images/Logo.svg";
-import linkedin from "../../assets/images/linkedin.svg"
-import twitter from "../../assets/images/twitter.svg"
-import facebook from "../../assets/images/facebook.svg"
-import instagram from "../../assets/images/instagram.svg"
+
 import Sidebar from "../sidebar/Sidebar";
 import { Link, useLocation } from "react-router-dom";
 import routes from "../../shared/constants/routes";
 import TelNumber from "../telnumber/TelNumber";
 import { useEffect, useRef, useState } from "react";
 import { useGetWindowWidth } from "../../shared/hooks/useGetWindowWith";
-import { Logo } from "../icons";
+import { Facebook, Instagram, LanguageIcon, Linkedin, Logo, Twitter } from "../icons";
 
 export const Header = () => {
     const x = useRef<any>()
     const y = useRef<any>()
+    const b = useRef<any>()
+    const l = useRef<any>()
     const widthwindow: any = useGetWindowWidth()
     const router: any = useLocation()
     const [scrollPosition, setScrollPosition] = useState<any>(0);
@@ -31,12 +30,19 @@ export const Header = () => {
         };
 
     }, []);
-    if (scrollPosition > 639 && y?.current) {
+    if (scrollPosition > 629 && y?.current && b?.current && l?.current) {
         y.current.style.background = "#ffffff"
+        b.current.style.background = "#3B42C5"
+        b.current.style.color = "#ffffff"
+        l.current.style.color = "black"
+
 
     }
-    if (scrollPosition < 639 && y?.current && router.pathname == '/') {
+    if (scrollPosition < 629 && y?.current && b?.current && l?.current && router.pathname == '/') {
         y.current.style.background = "transparent"
+        b.current.style.background = "#ffffff"
+        b.current.style.color = "#3B42C5"
+        l.current.style.color = "#ffffff"
 
     }
 
@@ -44,24 +50,31 @@ export const Header = () => {
         <>
             <header ref={y} className="header">
                 <div className="container_header">
-                    <Link to={routes.HOME}>  {scrollPosition < 639 && router.pathname == '/' ? <Logo fill={'#ffffff'} fill2={'#ffffff'} /> : <Logo fill={'#3B42C5'} fill2={'#57C5B6'} />}</Link>
-                    {widthwindow > 1145 ? <Sidebar /> : widthwindow > 500 ? <p className={`header-munutext ${color}`}>MENU</p> : ""}
+                    <Link to={routes.HOME}>  {scrollPosition < 629 && router.pathname == '/' ? <Logo fill={'#ffffff'} fill2={'#ffffff'} /> : <Logo fill={'#3B42C5'} fill2={'#57C5B6'} />}</Link>
+                    {widthwindow > 1145 ? <>
+                        {scrollPosition < 629 && router.pathname == '/' ? <Sidebar color={'white'} /> : <Sidebar color={'none'} />}
 
-                    <p className="header_language"> {widthwindow > 684 ? <>Русский</> : <>Ру</>}
-                        <ul className="wrapLaga">
-                            <li>O'zbekcha</li>
-                            <li>Turkce</li>
-                            <li>English</li>
-                            <li>Русский</li>
-                        </ul>
-                    </p>
+                    </> : widthwindow > 500 ? <p className={`header-munutext ${color}`}>MENU</p> : ""}
+
+                    <div className={'header_languagewrap'}>
+                        <LanguageIcon fill={scrollPosition < 629 && router.pathname == '/' ? "white" : "black"} />
+                        <p ref={l} className="header_language" >  {widthwindow > 684 ? <>  Русский</> : <>Ру</>}
+                            <ul className="wrapLaga">
+                                <li>O'zbekcha</li>
+                                <li>Turkce</li>
+                                <li>English</li>
+                                <li>Русский</li>
+                            </ul>
+                        </p>
+                    </div>
                     {widthwindow > 640 ? <>
-                        <button className="btn" onClick={() => x.current.style.display = "block"}>Оставить заявку</button>
+                        <button ref={b} className="btn" onClick={() => x.current.style.display = "block"}>Оставить заявку</button>
                         <div className="header_icons">
-                            <img src={linkedin} alt="icon" />
-                            <img src={twitter} alt="icon" />
-                            <img src={facebook} alt="icon" />
-                            <img src={instagram} alt="icon" />
+                            <Linkedin fill={scrollPosition < 629 && router.pathname == '/' ? "white" : "#3910CE"} />
+                            <Twitter fill={scrollPosition < 629 && router.pathname == '/' ? "white" : "#3910CE"} />
+                            <Facebook fill={scrollPosition < 629 && router.pathname == '/' ? "white" : "#3910CE"} />
+                            <Instagram fill={scrollPosition < 629 && router.pathname == '/' ? "white" : "#3910CE"} />
+
                         </div>
                     </> : <div className='header_burger'></div>
                     }
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom";
 import routes from "../../shared/constants/routes";
 
 
-export default function Sidebar() {
+export default function Sidebar({ color }) {
   return (
-    <ul className="SiteBar_list">
-      <li className="SiteBar_list-item"> <Link className="SiteBar_list-link" to={routes.ABOUTUS}>О компании</Link></li>
-      <li className="SiteBar_list-item"> <Link className="SiteBar_list-link" to={routes.SERVICES}>Услуги</Link></li>
-      <li className="SiteBar_list-item"> <Link className="SiteBar_list-link" to={routes.OBJECT}>Объекты</Link></li>
-      <li className="SiteBar_list-item"> <Link className="SiteBar_list-link" to={routes.NEWS}>Новости</Link></li>
-      <li className="SiteBar_list-item"> <Link className="SiteBar_list-link" to={routes.CONTACT}>Контакты</Link></li>
+    <ul className="SiteBar_list ">
+      <li className="SiteBar_list-item"> <Link className={`SiteBar_list-link ${color}`} to={routes.ABOUTUS}>О компании</Link></li>
+      <li className="SiteBar_list-item"> <Link className={`SiteBar_list-link ${color}`} to={routes.SERVICES}>Услуги</Link></li>
+      <li className="SiteBar_list-item"> <Link className={`SiteBar_list-link ${color}`} to={routes.OBJECT}>Объекты</Link></li>
+      <li className="SiteBar_list-item"> <Link className={`SiteBar_list-link ${color}`} to={routes.NEWS}>Новости</Link></li>
+      <li className="SiteBar_list-item"> <Link className={`SiteBar_list-link ${color}`} to={routes.CONTACT}>Контакты</Link></li>
     </ul>
 
   );
